22fdd3ae963cd1ca87155afed996812afc9097dc
Jack Lu <jacklu333333@gmail.com>
2023-09-19 00:37:47 +0900

    v0.1

--- IOHandler.py
+++ IOHandler.py
@@ -3,47 +3,61 @@ import numpy as np
 
 
 class IOHandler:
-    def __init__(self, map: np.array, latitudeStart: float, longitudeStart: float, latitudinalEnd: float, longitudinalEnd: float):
+    def __init__(self, map: np.array, latitudeStart: float, longitudeStart: float, latitudeEnd: float, longitudeEnd: float):
         self.map = map
         self.width, self.height = map.shape
+        # check the ending is larger than the starting
+        assert latitudeStart < latitudeEnd
+        assert longitudeStart < longitudeEnd
 
         # the latitude and longitude the top left corner of the map fill out the matching map
-        self.latitude = latitudeStart
-        self.longitude = longitudeStart
+        self.longitudeS = longitudeStart
+        self.latitudeS = latitudeStart
 
-        self.latitudinal_resolution = (
-            latitudinalEnd - latitudeStart) / self.width
+        self.longitudeE = longitudeEnd
+        self.latitudeE = latitudeEnd
+
         self.longitudinal_resolution = (
-            longitudinalEnd - longitudeStart) / self.height
-
-        self.latitudes = []
-        for i in range(self.width):
-            self.latitudes.append(
-                self.latitude + self.latitudinal_resolution * i)
+            longitudeEnd - longitudeStart) / self.width
+        self.latitudinal_resolution = (
+            latitudeEnd - latitudeStart) / self.height
 
         self.longitudes = []
-        for i in range(self.height):
+        for i in range(self.width):
             self.longitudes.append(
-                self.longitude + self.longitudinal_resolution * i)
+                self.longitudeS + self.longitudinal_resolution * i)
+        self.longitudes = np.array(self.longitudes)
+        self.latitudes = []
+
+        for i in range(self.height):
+            self.latitudes.append(
+                self.latitudeS + self.latitudinal_resolution * i)
+        self.latitudes = np.array(self.latitudes)
 
     def map2latlon(self, x, y):
-        return self.latitudes[x], self.longitudes[y]
+        return self.latitudes[y], self.longitudes[x]
 
     def latlon2map(self, lat, lon):
         # find the closest latitude and longitude
-        x = np.argmin(np.abs(self.latitudes - lat))
-        y = np.argmin(np.abs(self.longitudes - lon))
+        y = np.argmin(np.abs(self.latitudes - lat))
+        x = np.argmin(np.abs(self.longitudes - lon))
         return x, y
 
-    def searchValid(self, lontitude, latitude):
+    def searchValid(self, latitude: float, lontitude: float):
+        # check if the coordinate is in the map
+        if latitude < self.latitudeS or latitude > self.latitudeE or lontitude < self.longitudeS or lontitude > self.longitudeE:
+            print("The coordinate is not in the map")
+            return False
+
         x, y = self.latlon2map(latitude, lontitude)
 
         return self.map[x, y]
 
 
 def recursivePadding(img: np.array, padding_value: int, x: int, y: int):
+    # print(x, y, padding_value)
     # check if the padding value is 0
-    if padding_value == 0:
+    if padding_value <= 0:
         return
 
     # check if the coordinate is in img array
@@ -51,28 +65,32 @@ def recursivePadding(img: np.array, padding_value: int, x: int, y: int):
         return
 
     # check if the coordinate is already padded
-    if img[x, y] != 0 and img[x, y] >= padding_value:
+    if img[x, y] != 0 and img[x, y] > padding_value:
         return
 
     img[x, y] = padding_value
 
-    recursivePadding(img, padding_value-0.1, x - 1, y)
-    recursivePadding(img, padding_value-0.1, x + 1, y)
-    recursivePadding(img, padding_value-0.1, x, y - 1)
-    recursivePadding(img, padding_value-0.1, x, y + 1)
+    recursivePadding(img, padding_value-0.01, x - 1, y)
+    recursivePadding(img, padding_value-0.01, x + 1, y)
+    recursivePadding(img, padding_value-0.01, x, y - 1)
+    recursivePadding(img, padding_value-0.01, x, y + 1)
 
 
-def image2numpy(dir2img: str, entrance: list=[]):
+def image2numpy(dir2img: str, entrance: list = []):
     img = Image.open(dir2img)
 
     # convert gray scale
     img = img.convert('L')
 
+    # corp the image to where the numpy array is not null
+    img = img.crop(img.getbbox())
+
     # convert to numpy array
     numpy_data = np.array(img)
+    numpy_data = np.invert(numpy_data.astype(np.bool_)).astype(np.float32)
 
     # for each entrace contain a pair of x and y
     for x, y in entrance:
-        recursivePadding(numpy_data, 255, x, y)
+        recursivePadding(numpy_data, 0.9, x, y)
 
     return numpy_data
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # IOHandler
 [https://wooiljeong.github.io/python/static-map/]
+[https://pygis.io/docs/d_access_osm.html]
